ebcf1bd15646875cb52de8b6ec1b090a7562f2f7
Tamar Cohen <tamar.cohen@nasa.gov>
2017-02-15 15:16:01 -0800

    FINALLY fixed map size and resizing and stuff on the bottom holy COW

--- xgds_core/static/xgds_core/js/gridstackUtils.js
+++ xgds_core/static/xgds_core/js/gridstackUtils.js
@@ -53,18 +53,6 @@ $.extend(xgds_gridstack,{
 	bindMapResize: function (container){
 		var mapGridstackItem = container.find("#map-gridstack-item");
 		if (mapGridstackItem.length == 1) {
-			// disable resize on the map
-			var foundMap = mapGridstackItem.find("#map");
-			if (foundMap.length > 0){
-				if (foundMap.resizable !== undefined){
-					try {
-						foundMap.resizable('destroy');
-					} catch (err){
-						//gulp, in case we try to destroy resizing before it was initialized
-					}
-				}
-			}
-//			mapGridstackItem.find("#map").resizable("disable").removeClass('ui-state-disabled');;
 			mapGridstackItem.on('resizestop', function(event, ui){
 				app.vent.trigger('doMapResize');
 			});
