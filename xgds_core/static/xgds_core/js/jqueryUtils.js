b40d5d38d07cfd3a3ad1108047d660b3485f78bb
Tamar Cohen <tamar.cohen@nasa.gov>
2017-02-21 17:24:46 -0800

    added arrayEquals

--- xgds_core/static/xgds_core/js/jqueryUtils.js
+++ xgds_core/static/xgds_core/js/jqueryUtils.js
@@ -70,5 +70,67 @@ $.extend({
 	},
 	getValueByName: function(functionName) {
 		return eval(functionName);
-	}
+	},
+	arrayEquals: function(a1, a2){
+	    return (JSON.stringify(a1) == JSON.stringify(a2));
+	},
+	indexBy: function(list, keyProp) {
+		// Return an object that indexes the objects in a list by their key property.
+		// keyProp should be a string.
+		obj = {};
+		_.each(list, function(item) {
+			obj[item[keyProp]] = item;
+		});
+		return obj;
+	},
+	groupBy: function(list, keyProp) {
+		obj = {};
+		_.each(list, function(item) {
+			if (_.isUndefined(obj[item[keyProp]])) {
+				obj[item[keyProp]] = [];
+			}
+			obj[item[keyProp]].push(item);
+		});
+		return obj;
+	},
+	randomColor: function() {
+        return '#' + ((1 << 24) * Math.random() | 0).toString(16);
+    },
+    rainbow: function(numOfSteps, step) {
+        // This function generates vibrant, 'evenly spaced' colours (i.e. no clustering).
+        // This is ideal for creating easily distiguishable vibrant markers in Google Maps and other apps.
+        // Adam Cole, 2011-Sept-14
+        // HSV to RBG adapted from:
+        // http://mjijackson.com/2008/02/rgb-to-hsl-and-rgb-to-hsv-color-model-conversion-algorithms-in-javascript
+        // source: http://stackoverflow.com/questions/1484506/random-color-generator-in-javascript/7419630
+        var r, g, b;
+        var h = step / numOfSteps;
+        var i = ~~(h * 6);
+        var f = h * 6 - i;
+        var q = 1 - f;
+        switch (i % 6) {
+        case 0:
+            r = 1, g = f, b = 0;
+            break;
+        case 1:
+            r = q, g = 1, b = 0;
+            break;
+        case 2:
+            r = 0, g = 1, b = f;
+            break;
+        case 3:
+            r = 0, g = q, b = 1;
+            break;
+        case 4:
+            r = f, g = 0, b = 1;
+            break;
+        case 5:
+            r = 1, g = 0, b = q;
+            break;
+        }
+        var c = '#' + ('00' + (~~(r * 255)).toString(16)).slice(-2) +
+            ('00' + (~~(g * 255)).toString(16)).slice(-2) +
+            ('00' + (~~(b * 255)).toString(16)).slice(-2);
+        return (c);
+    },
 });
