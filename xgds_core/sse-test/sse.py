1fa1ed1bcb701e9f8fcb56bf43c7fa890d8c0699
David Lees <david.lees@nasa.gov>
2017-04-26 18:31:38 +0000

    Add SSE test scripts in sse-test

---
+++ xgds_core/sse-test/sse.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+from flask_sse import sse
+from flask import request
+
+app = Flask(__name__)
+app.config["REDIS_URL"] = "redis://localhost"
+app.register_blueprint(sse, url_prefix='/stream')
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/hello')
+def publish_hello():
+    argString = request.args.get("argString", "")
+    sse.publish({"message": "Hello %s!" % argString}, type='greeting')
+    return "Message sent!"
