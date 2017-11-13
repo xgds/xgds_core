2a91d0bae284c6dc2276b54d0134119278577c67
Tamar Cohen <tamar.cohen@nasa.gov>
2017-11-12 20:10:18 -1000

    fixed path to redis socks

--- xgds_core/sse/server-sse.py
+++ xgds_core/sse/server-sse.py
@@ -2,5 +2,5 @@ from flask import Flask
 from flask_sse import sse
 
 app = Flask(__name__)
-app.config["REDIS_URL"] = "unix:///var/run/redis.sock"
+app.config["REDIS_URL"] = "unix:///var/run/redis/redis.sock"
 app.register_blueprint(sse, url_prefix='/stream')
