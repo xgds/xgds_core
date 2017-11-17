3dd7cf5c714dc0a811dba60de4877c610421bef9
Tamar Cohen <tamar.cohen@nasa.gov>
2017-11-17 09:38:46 -1000

    added owa tracking function callback

--- xgds_core/static/xgds_core/js/owaTrackingInclude.js
+++ xgds_core/static/xgds_core/js/owaTrackingInclude.js
@@ -1,5 +1,20 @@
+//__BEGIN_LICENSE__
+// Copyright (c) 2015, United States Government, as represented by the
+// Administrator of the National Aeronautics and Space Administration.
+// All rights reserved.
+//
+// The xGDS platform is licensed under the Apache License, Version 2.0
+// (the "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0.
+//
+// Unless required by applicable law or agreed to in writing, software distributed
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the
+// specific language governing permissions and limitations under the License.
+//__END_LICENSE__
+
 <!-- Start Open Web Analytics Tracker -->
-//<![CDATA[
 var owa_baseUrl = owaUrlFromTemplate;
 var owa_cmds = owa_cmds || [];
 owa_cmds.push(['setSiteId', owaSiteIdFromTemplate]);
@@ -11,6 +26,17 @@ owa_cmds.push(['trackClicks']);
 	owa_baseUrl = ('https:' == document.location.protocol ? window.owa_baseSecUrl || owa_baseUrl.replace(/http:/, 'https:') : owa_baseUrl );
 	_owa.src = owa_baseUrl + 'modules/base/js/owa.tracker-combined-min.js';
 	var _owa_s = document.getElementsByTagName('script')[0]; _owa_s.parentNode.insertBefore(_owa, _owa_s);
+
 }());
-//]]>
+
+var owaTrackAction = function(category, action, identifier) {
+	// Category = object type
+	// Action = action taken ie view details, edit
+	// Identifier == something to identify object, ie pk & name
+	OWATracker.trackAction(category, action,  identifier);
+	return true;
+}
+
+
+
 <!-- End Open Web Analytics Code -->
