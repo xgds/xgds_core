0ac8304c4356df7b017d9ad4ad0b25ebc5ee1c54
Brian Atiyeh <btatiyeh@gmail.com>
2018-01-31 21:28:39 -0500

    Code cleanup for filtering search data

--- xgds_core/views.py
+++ xgds_core/views.py
@@ -213,6 +213,12 @@ class OrderListJson(BaseDatatableView):
             self.queries |= query
         else:
             self.queries = query
+
+    def addAndQuery(self, query):
+        if self.formQueries:
+            self.formQueries &= query
+        else:
+            self.formQueries = query
         
     def buildQuery(self, search):
         self.queries = None
@@ -296,31 +302,18 @@ class OrderListJson(BaseDatatableView):
             if noteQuery:
                 qs = qs.filter(noteQuery)
 
-        last = self.request.POST.get(u'last', -1)
-        if last > 0:
-            qs = qs[-last:]
-
         return qs.distinct()
 
     def filter_queryset_advanced_search(self, qs, searchDict):
         if searchDict:
             for key in searchDict:
-                self.buildAdvancedQuery(key, searchDict[key])
+                if (unicode(searchDict[key]).isnumeric()):
+                    self.addAndQuery(Q(**{key: searchDict[key]}))
+                else:
+                    self.addAndQuery(Q(**{key + '__icontains': searchDict[key]}))
             qs = qs.filter(self.formQueries)
 
         return qs.distinct()
-
-    def buildAdvancedQuery(self, key, value):
-        if (unicode(value).isnumeric()):
-            self.addAndQuery(Q(**{key:value}))
-        else:
-            self.addAndQuery(Q(**{key + '__icontains': value}))
-
-    def addAndQuery(self, query):
-        if self.formQueries:
-            self.formQueries &= query
-        else:
-            self.formQueries = query
     
 def helpPopup(request, help_content_path, help_title):
     return render(request,
