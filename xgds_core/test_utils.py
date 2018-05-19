56ac2479fd2876b34f7b274ed60853872f307535
Matthew Deans <matthew.deans@nasa.gov>
2018-05-19 01:02:02 +0000

    adding more tests

--- xgds_core/test_utils.py
+++ xgds_core/test_utils.py
@@ -20,9 +20,13 @@ import pytz
 from django.conf import settings
 from django.test import TestCase
 
-from xgds_core.flightUtils import get_default_vehicle, getFlight, getNextAlphabet, create_group_flight
+from xgds_core.flightUtils import get_default_vehicle
+from xgds_core.flightUtils import getFlight, getActiveFlight
+from xgds_core.flightUtils import getNextAlphabet
+from xgds_core.flightUtils import create_group_flight, get_next_available_group_flight_name
+from xgds_core.flightUtils import lookup_vehicle, lookup_flight
 
-class xgds_getFlightTest(TestCase):
+class xgds_AllTheUtils(TestCase):
     """
     Tests for getFlight
     """
@@ -34,20 +38,17 @@ class xgds_getFlightTest(TestCase):
         assert(flight.uuid=='abc123')
         assert(flight.name=='Christmas in a Generic Vehicle')
 
-
     def test_getFlightWithoutVehicle(self):
         flight = getFlight(datetime.datetime(2015,12,25,12,0,0,0,pytz.utc),None)
         assert(flight.id==1)
         assert(flight.uuid=='abc123')
         assert(flight.name=='Christmas in a Generic Vehicle')
 
-
     def test_getFlightNoResult(self):
         # If there is no vehicle and the query time is PRIOR TO the
         # active flight we should get None as a result
         flight = getFlight(datetime.datetime(2014,12,25,12,0,0,0,pytz.utc),None)
         assert(flight==None)
-
 
     def test_getFlightActive(self):
         # If there is no vehicle and the query time is AFTER the
@@ -56,7 +57,6 @@ class xgds_getFlightTest(TestCase):
         assert(flight.id==1)
         assert(flight.uuid=='abc123')
         assert(flight.name=='Christmas in a Generic Vehicle')
-
 
     def test_getNextAlphabet(self):
         assert('b'==getNextAlphabet('a'))
@@ -69,3 +69,49 @@ class xgds_getFlightTest(TestCase):
         # assert('C'==getNextAlphabet('B'))
         # assert('A'==getNextAlphabet('Z'))
 
+    def test_getActiveFlight(self):
+        flight = getActiveFlight(1)
+        assert(flight.id==1)
+        assert(flight.uuid=='abc123')
+        assert(flight.name=='Christmas in a Generic Vehicle')
+
+    def test_getActiveFlightNoVehicle(self):
+        flight = getActiveFlight(None)
+        assert(flight.id==1)
+        assert(flight.uuid=='abc123')
+        assert(flight.name=='Christmas in a Generic Vehicle')
+
+    def test_getActiveFlightNoResult(self):
+        flight = getActiveFlight(2)
+        assert(flight==None)
+
+    def test_getNextAvailableGroupFlightName(self):
+        name = get_next_available_group_flight_name('Pre-')
+        print 'next available name is "%s"' % name
+        assert(name=='Pre-A')
+        name = get_next_available_group_flight_name('test-')
+        print 'next available name is "%s"' % name
+        assert(name=='test-B')
+
+    def test_get_default_vehicle(self):
+        vehicle = get_default_vehicle()
+        assert(vehicle.name=='Generic Vehicle')
+
+    def test_create_group_flight(self):
+        vehicle = get_default_vehicle()
+        gf = create_group_flight("my group flight","no notes")
+        assert(gf is not None)
+        gf = create_group_flight("my second group flight","no notes",[vehicle])
+        assert(gf is not None)
+
+    def test_lookup_vehicle(self):
+        v1 = lookup_vehicle('Generic Vehicle')
+        v2 = lookup_vehicle('Batmobile')
+        print 'vehicle 1 = %s' % v1
+        #print 'vehicle 2 = %s' % v2
+        assert(v1 is not None)
+        #assert(v2 is None)
+
+    def test_lookup_flight(self):
+        f1 = lookup_flight('Christmast in a Generic Vehicle')
+        f2 = lookup_flight('Christmast in the Batmobile')
