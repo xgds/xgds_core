cb8e5ac031ceb46b3cda44ec0d1590098cce2f1b
Khaled Sharif <kldsrf@gmail.com>
2019-03-06 12:37:43 -0800

    [sse] fixed the sse starting script (flask_sse)

--- xgds_core/sse/sse.py
+++ xgds_core/sse/sse.py
@@ -2,5 +2,9 @@ from flask import Flask
 from flask_sse import sse
 
 app = Flask(__name__)
-app.config["REDIS_URL"] = "redis://localhost"
-app.register_blueprint(sse, url_prefix='/stream')
+app.config["REDIS_URL"] = "redis://redis"
+app.register_blueprint(sse, url_prefix='/sse/stream')
+
+@app.route('/sse/status')
+def send_message():
+    return "SSE is working!"
